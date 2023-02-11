8579f344b402ac77e682db2ff3a1ba1d2aecb142
Duy Anh Do <doduyanh149@gmail.com>
2023-02-11 19:57:50 +0700

    hello

---
+++ home/home.js
@@ -0,0 +1,4 @@
+// git status : kiểm tra trạng thái
+// git add . : đẩy tất cả các file thay đổi vào trong vùng stage
+// git commit -m"Nội dung muốn thông báo"
+// git push : đẩy tất cả các file lên git
